9df2bca60317c6a9680a3ed18405c5bfd44d7aed
ErixTheUncoder <erichong.0969@gmail.com>
2025-08-27 15:53:17 +0800

    first Initial

---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(){
+return(
+  <div className="card">
+    <img src="./assets/react.svg"></img>
+    <h2>Eric</h2>
+    <p>I am a university student studying in MMU</p>
+  </div>
+)
+
+}
+export default Card
