ed53308775a5c3e77595a8f82cbdbd0390811729
ErixTheUncoder <erichong.0969@gmail.com>
2025-08-27 19:08:48 +0800

    added a button and changed it's location

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import Header from "./Header";
-import Footer from "./Footer";
-import Food from "./Food";
-import Card from "./Card";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Food from "./components/Food";
+import Card from "./components/Card";
+import Button from "./Button/Button";
 
 function App() {
   return (
     <>
       <Header />
       <Food />
-      <Card />
+      <div>
+        <Card />
+      </div>
+      <Button />
       <Footer />
     </>
   );
