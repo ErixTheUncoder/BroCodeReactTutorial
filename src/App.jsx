8c0d2ec75ef443861736258b2f99c180c697a375
ErixTheUncoder <erichong.0969@gmail.com>
2025-08-28 00:06:28 +0800

    props

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Food from "./components/Food";
 import Card from "./components/Card";
-import Button from "./Button/Button";
+import Button from "./components/Button/Button";
+import Student from "./components/Student";
 
 function App() {
   return (
@@ -11,6 +12,12 @@ function App() {
       <Food />
       <div>
         <Card />
+      </div>
+      <div>
+        <Student name="Spongebob" age={30} isStudent={true}/>
+        <Student name="Patrick" age={42} isStudent={false} />
+        <Student name="Squidward" age = {50} isStudent={false} />
+        <Student name="Sandy" age={27} isStudent={true}/>
       </div>
       <Button />
       <Footer />
