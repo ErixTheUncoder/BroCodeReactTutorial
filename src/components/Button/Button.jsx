b04836839f3a0b30b8bb835b9ddc99eeaaf097d7
ErixTheUncoder <erichong.0969@gmail.com>
2025-09-03 12:09:20 +0800

    updated current progress in readme.md

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,19 @@
 import styles from "./Button.module.css";
 
 function Button(){
+  let count = 0;
+  const HandleClick = (name) => {
+    if(count < 3){
+      count++;
+      console.log(`${name} have clicked me ${count}`);
+    }
+    else{
+      console.log(`${name}, please stop clicking me.`);
+    }
+  };
+
   return(
-    <button className={styles.button}>Click Me</button>
+    <button className={styles.button} onClick={()=>HandleClick("User")}>Click Me</button>
   );
 }
 export default Button
